42fd2c987a8ee25b600165a03be217cd0c35b0c6
Aziret055 <kojom5erd1ev1@gmail.com>
2024-04-20 15:58:31 +0600

    geeks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import './App.css'
+import Image from './components/Image'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [datas, setDatas] = useState([])
+	function getProduct() {
+		axios(`https://pokeapi.co/api/v2/pokemon/`).then(res => {
+			setDatas(res.data.results)
+		})
+	}
+
+	useEffect(() => {
+		getProduct()
+	}, [])
+	console.log(datas, 'datas')
+	return (
+		<div className='App'>
+			<div className='header container'>
+				<div className='neader_nav'>
+					<div className='top'>
+						<div className='topH1'>
+							<h1>GEEKS</h1>
+						</div>
+						<div className='fff'>
+							{datas.slice(0, 6).map(el => (
+								<div className='block'>
+									<Image url={el.url} key={el.url} />
+									<h3>{el.name}</h3>
+								</div>
+							))}
+						</div>
+					</div>
+					<div className=''></div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
